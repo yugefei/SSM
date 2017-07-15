b8c06eb92f69648da4210121e031dde83337322b
gefeiyu <gefeiyu@tencent.com>
2017-07-16 02:55:03 +0800

    a

--- src/main/java/com/tencent/seventeenShow/backend/controller/vo/ChangeResumeVo.java
+++ src/main/java/com/tencent/seventeenShow/backend/controller/vo/ChangeResumeVo.java
@@ -9,7 +9,15 @@ public class ChangeResumeVo {
     private String username;
     private String gender;
     private  String birthday;
+    private String avatar;
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
     public String getBirthday() {
         return birthday;
